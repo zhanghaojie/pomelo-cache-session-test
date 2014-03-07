b6d788388240236cdd7386343b7e162f1b6468c7
zhanghaojie <zhj.linqsoft@gmail.com>
2014-03-07 20:40:57 +0800

    save

---
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -0,0 +1,20 @@
+module.exports = function(app) {
+  return new Handler(app);
+};
+
+var Handler = function(app) {
+  this.app = app;
+};
+
+/**
+ * New client entry chat server.
+ *
+ * @param  {Object}   msg     request message
+ * @param  {Object}   session current session object
+ * @param  {Function} next    next stemp callback
+ * @return {Void}
+ */
+Handler.prototype.entry = function(msg, session, next) {
+  session.bind("admin");
+  next(null, {code: 200, msg: 'game server is ok.'});
+};
